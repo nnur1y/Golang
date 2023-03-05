55a6ce8bd9d26c6ad676dd695acd9d7c84e61696
dio.ni <200103264@stu.sdu.edu.kz>
2023-03-05 17:39:58 +0600

    delete some files

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -88,4 +91,35 @@ func handlerUserAuthorization(c *gin.Context) {
 	// c.JSON(200, gin.H{
 	// 	"Error": nil,
 	// })
+}
+
+type User struct {
+	ID        int    `json:"ID,-"`
+	Login     string `json:"login"`
+	Password  string `json:"Password,-"`
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	Role      string `json:"Role,-"`
+}
+
+// Create создание нового пользователя в базе
+func (u User) Create() error {
+	{ // Insert a new user
+
+		username := "FirstName"
+		password := "Password"
+		createdAt := time.Now()
+		db, err := sql.Open("mysql", "root:password@(localhost:3306)/world?parseTime=true")
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+		fmt.Println(id)
+	}
+
+	fmt.Println("Create new user with ID", u.ID)
+
+	return nil
 }
