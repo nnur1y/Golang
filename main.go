ea8777e7d1bf7e9a37a870688bf60c3a5efafa3f
nnur1y <nurly125@mail.ru>
2023-03-20 23:04:31 +0600

    last

--- main.go
+++ main.go
@@ -8,12 +8,23 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var router *gin.Engine
+var store = sessions.NewCookieStore([]byte("super-secret"))
+
+type Recipe struct {
+	Id         int
+	Name       string
+	Definition string
+	Author     string
+}
+
+var searchText string
 
 func main() {
 	var e error
@@ -36,13 +47,75 @@ func main() {
 	router.GET("/authorization", handlerAuthorization)
 	router.POST("/user/reg", handlerUserRegistration)
 	router.POST("/user/auth", handlerUserAuthorization)
-	router.POST("/log")
 	authRouter.GET("/profile", profileHandler)
 
 	_ = router.Run(":8080")
 }
 
+var RecipesList []Recipe
+
+func handleSearch(c *gin.Context) {
+	c.HTML(200, "layout.html", gin.H{
+		"error":   false,
+		"content": RecipesList,
+	})
+}
+
+func auth(c *gin.Context) {
+	fmt.Println("auth middleware running")
+	session, _ := store.Get(c.Request, "session")
+	fmt.Println("session:", session)
+	_, ok := session.Values["user"]
+	if !ok {
+		c.HTML(http.StatusForbidden, "registration.html", nil)
+		c.Abort()
+		return
+	}
+	fmt.Println("middleware done")
+	c.Next()
+}
+
+func profileHandler(c *gin.Context) {
+	session, _ := store.Get(c.Request, "session")
+	var user = &User{}
+	val := session.Values["user"]
+	var ok bool
+	if user, ok = val.(*User); !ok {
+		fmt.Println("was not of type *User")
+		c.HTML(http.StatusForbidden, "authorization.html", nil)
+		return
+	}
+	c.HTML(http.StatusOK, "profile.html", gin.H{"user": user})
+}
+
+// pkg.go.dev/text/template
+func handlerIndexMore(c *gin.Context) {
+
+	var searchItem SearchItem
+	e := c.BindJSON(&searchItem)
+	if e != nil {
+		fmt.Print(e)
+	}
+	e = searchItem.Search()
+	if e != nil {
+		fmt.Println("Logged in!")
+		c.HTML(200, "layout.html", gin.H{
+			"error":   false,
+			"content": RecipesList,
+		})
+		c.Request.Response.Location()
+		return
+	} else {
+
+		c.HTML(200, "layout.html", gin.H{
+			"error":   true,
+			"content": RecipesList,
+		})
+		return
+	}
+}
 func handlerIndex(c *gin.Context) {
+
 	db, _ := sql.Open("mysql", "root:password@(localhost:3306)/world?parseTime=true")
 	result, err := db.Query("SELECT  * from Recipe")
 	if err != nil {
@@ -80,51 +153,77 @@ func handlerIndex(c *gin.Context) {
 	})
 
 }
+func handlerRegistration(c *gin.Context) {
+	c.HTML(200, "registration.html", gin.H{})
+}
 
-// pkg.go.dev/text/template
-func handlerIndexMore(c *gin.Context) {
+func handlerAuthorization(c *gin.Context) {
+	c.HTML(200, "authorization.html", gin.H{})
+}
 
-	var searchItem SearchItem
-	e := c.BindJSON(&searchItem)
+func handlerUserRegistration(c *gin.Context) {
+
+	var user User
+	e := c.BindJSON(&user)
+
 	if e != nil {
-		fmt.Print(e)
-	}
-	e = searchItem.Search()
-	if e != nil {
-		fmt.Println("Logged in!")
-		c.HTML(200, "layout.html", gin.H{
-			"error":   false,
-			"content": RecipesList,
+		c.JSON(200, gin.H{
+			"Error": e.Error(),
 		})
-		c.Request.Response.Location()
-		return
+
+	}
+
+	e = user.Create()
+	fmt.Println(e)
+	if e != nil {
+		c.JSON(200, gin.H{
+			"Error": "Не удалось зарегистрировать пользователя",
+		})
+		c.Redirect(http.StatusFound, "/authorization")
+
 	} else {
-
-		c.HTML(200, "layout.html", gin.H{
-			"error":   true,
-			"content": RecipesList,
-		})
-		return
+		fmt.Println("Signed up!")
 	}
+
 }
 
-var store = sessions.NewCookieStore([]byte("super-secret"))
+func handlerUserAuthorization(c *gin.Context) {
+	var user User
+	e := c.BindJSON(&user)
+	if e != nil {
+		c.JSON(200, gin.H{
+			"Error": e.Error(),
+		})
+		fmt.Println("Некорректные данные")
+	} else {
+		e = user.Select()
+		if e != nil {
+			//incorrect email or password
+			c.HTML(200, "authorization.html", gin.H{"message": "incorrect username or password"})
+		} else {
+			session, _ := store.Get(c.Request, "session")
+			// session struct has field Values map[interface{}]interface{}
+			session.Values["user"] = user
+			// save before writing to response/return from handler
+			session.Save(c.Request, c.Writer)
 
-type Recipe struct {
-	Id         int
-	Name       string
-	Definition string
-	Author     string
-}
+			// correct email and password
+			fmt.Println(session)
+			c.HTML(200, "loggedin.html", gin.H{"email": user.Email})
 
-var searchText string
-var RecipesList []Recipe
-
-func handleSearch(c *gin.Context) {
-	c.HTML(200, "layout.html", gin.H{
-		"error":   false,
-		"content": RecipesList,
+		}
+	}
+	c.HTML(200, "authorization.html", gin.H{
+		"login":   true,
+		"message": "incorrect username or password",
 	})
+}
+
+type User struct {
+	Email     string `json:"Email"`
+	Password  string `json:"Password"`
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
 }
 
 type SearchItem struct {
@@ -132,9 +231,12 @@ type SearchItem struct {
 }
 
 func (s SearchItem) Search() error {
+
 	searchText = "%" + s.SearchItem + "%"
 	db, _ := sql.Open("mysql", "root:password@(localhost:3306)/world?parseTime=true")
+
 	result, err := db.Query("SELECT * FROM recipe WHERE name LIKE ? ", searchText)
+	// result, err2 := db.Query("SELECT  username, password FROM users WHERE username = ?", username)
 	if err != nil {
 		panic(err)
 	}
@@ -167,103 +269,6 @@ func (s SearchItem) Search() error {
 
 }
 
-type User struct {
-	Email     string `json:"Email"`
-	Password  string `json:"Password"`
-	FirstName string `json:"FirstName"`
-	LastName  string `json:"LastName"`
-}
-
-func auth(c *gin.Context) {
-	fmt.Println("auth middleware running")
-	session, _ := store.Get(c.Request, "session")
-	fmt.Println("session:", session)
-	_, ok := session.Values["user"]
-	if !ok {
-		c.HTML(http.StatusForbidden, "registration.html", nil)
-		c.Abort()
-		return
-	}
-	fmt.Println("middleware done")
-	c.Next()
-}
-
-func profileHandler(c *gin.Context) {
-	session, _ := store.Get(c.Request, "session")
-	var user = &User{}
-	val := session.Values["user"]
-	var ok bool
-	if user, ok = val.(*User); !ok {
-		fmt.Println("was not of type *User")
-		c.HTML(http.StatusForbidden, "authorization.html", nil)
-		return
-	}
-	c.HTML(http.StatusOK, "profile.html", gin.H{"user": user})
-}
-
-func handlerRegistration(c *gin.Context) {
-	c.HTML(200, "registration.html", gin.H{})
-}
-
-func handlerAuthorization(c *gin.Context) {
-	c.HTML(200, "authorization.html", gin.H{})
-}
-
-func handlerUserRegistration(c *gin.Context) {
-
-	var user User
-	e := c.BindJSON(&user)
-
-	if e != nil {
-		c.JSON(200, gin.H{
-			"Error": e.Error(),
-		})
-
-	}
-
-	e = user.Create()
-	//fmt.Println(e)
-	if e != nil {
-		c.JSON(200, gin.H{
-			"Error": "Не удалось зарегистрировать пользователя",
-		})
-		c.Redirect(http.StatusFound, "/authorization")
-
-	} else {
-		fmt.Println("Signed up!")
-		c.Redirect(http.StatusOK, "/layout.html")
-	}
-
-}
-
-func handlerUserAuthorization(c *gin.Context) {
-	var user User
-	e := c.BindJSON(&user)
-	if e != nil {
-		c.JSON(200, gin.H{
-			"Error": e.Error(),
-		})
-		fmt.Println("Некорректные данные")
-	} else {
-		e = user.Select()
-		if e != nil {
-			//incorrect email or password
-			c.HTML(200, "authorization.html", gin.H{"message": "incorrect username or password"})
-		} else {
-			session, _ := store.Get(c.Request, "session")
-			// session struct has field Values map[interface{}]interface{}
-			session.Values["user"] = user
-			// save before writing to response/return from handler
-			session.Save(c.Request, c.Writer)
-
-			// correct email and password
-			fmt.Println(session)
-			c.HTML(200, "loggedin.html", gin.H{"email": user.Email})
-
-		}
-	}
-}
-
 // Create создание нового пользователя в базе
 func (u User) Create() error {
 	{ // Insert a new user
@@ -281,6 +286,7 @@ func (u User) Create() error {
 		}
 
 		fmt.Println(result)
+
 		fmt.Println(email, password, firstname, lastname, createdAt)
 	}
 	return nil
