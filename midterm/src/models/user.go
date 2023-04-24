3b5a3f27dc744ac5e46e5a16d17ed9444bd386f9
nnur1y <nurly125@mail.ru>
2023-04-24 15:21:34 +0600

    last

--- midterm/src/models/user.go
+++ midterm/src/models/user.go
@@ -3,60 +3,97 @@ package models
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/nnur1y/Golang/tree/main/midterm/src/config"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Email     string `json:"Email"`
-	Password  string `json:"Password"`
-	FirstName string `json:"FirstName"`
-	LastName  string `json:"LastName"`
+	Id       string `json:"Id"`
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+	Password string `json:"Password"`
 }
 
 // Create создание нового пользователя в базе
-func (u User) Create() error {
-	{ // Insert a new user
+func (u *User) Create() error {
+	db, _ := config.LoadDB()
+	var hash []byte
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("bcrypt err:", err)
+		return err
+	}
 
-		email := u.Email
-		password := u.Password
-		firstname := u.FirstName
-		lastname := u.LastName
-		createdAt := time.Now()
-
-		db, _ := config.LoadDB()
-		result, err := db.Exec(`INSERT INTO users (email, password, username, lastname, created_at) VALUES (?, ?, ?, ?, ?)`, email, password, firstname, lastname, createdAt)
-		if err != nil {
-			log.Fatal(err)
+	var tx *sql.Tx
+	tx, err = db.Begin()
+	if err != nil {
+		fmt.Println("failed to begin transaction, err:", err)
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			fmt.Println("there was an error calling back changes, rollbackErr:", rollbackErr)
 		}
+		return err
+	}
+	defer tx.Rollback()
+	var insertStmt *sql.Stmt
+	insertStmt, err = tx.Prepare("INSERT INTO users (username, email, password) VALUES (?,?,?);")
+	if err != nil {
+		fmt.Println("error preparing statement:", err)
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			fmt.Println("there was an error rolling back changes, rollbackErr:", rollbackErr)
+			return err
 
-		fmt.Println(result)
+		}
+		defer insertStmt.Close()
+	}
 
-		fmt.Println(email, password, firstname, lastname, createdAt)
+	var result sql.Result
+	result, err = insertStmt.Exec(u.Username, u.Email, hash)
+	aff, _ := result.RowsAffected()
+	if aff == 0 {
+		return err
+	}
+	if err != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			fmt.Println("there was an error rolling back changes, rollbackErr:", rollbackErr)
+			return err
+
+		}
+		return err
+	}
+
+	if commitErr := tx.Commit(); commitErr != nil {
+		fmt.Println("error commiting changes:", err)
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			fmt.Println("there was an error rolling back changes, rollbackErr:", rollbackErr)
+		}
+		return err
 	}
 	return nil
 }
 
-func (u User) Select() error {
-	{ // Insert a new user
-
-		email := u.Email
-		password := u.Password
-
-		var (
-			getemail    string
-			getpassword string
-		)
-		db, err := sql.Open("mysql", "root:password@(localhost:3306)/world?parseTime=true")
-		query := "SELECT email, password FROM users WHERE email = ? and password = ?"
-		if err := db.QueryRow(query, email, password).Scan(&getemail, &getpassword); err != nil {
-			fmt.Println("Incorrect email or password")
-			return err
-		}
-
-		fmt.Println("Correct, Logged in")
+func (u *User) Select() error {
+	db, _ := config.LoadDB()
+	query := "SELECT * FROM users WHERE username = ?"
+	err := db.QueryRow(query, u.Username).Scan(&u.Id, &u.Username, &u.Email, &u.Password)
+	if err != nil {
+		fmt.Println("getUser() error selecting User, err:", err)
 		return err
 	}
+
+	fmt.Println("Correct, Logged in")
+	return nil
+}
+
+func (u *User) UsernameExists() (exists bool) {
+	exists = true
+	db, _ := config.LoadDB()
+	stmt := "SELECT Id FROM users WHERE Username = ?"
+	row := db.QueryRow(stmt, u.Username)
+	var uId string
+	err := row.Scan(&uId)
+	if err == sql.ErrNoRows {
+		return false
+	}
+	return exists
 }
